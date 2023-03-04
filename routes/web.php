030927a0eaca83352e6da667bbbc365b417b35de
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2023-03-04 12:17:45 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,87 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [];
+    $data[] = [
+        'url' => 'api/users/insert',
+        'request' => '{
+    "name": "gzeinnumer3",
+    "email": "gzeinnumer3@admin.com",
+    "password": "12345678"
+}',
+        'response' => '{
+    "status": 1,
+    "title": "Perhatian",
+    "message": "Success"
+}'
+    ];
+    $data[] = [
+        'url' => 'api/users/all',
+        'request' => '-',
+        'response' => '{
+    "status": 1,
+    "title": "Perhatian",
+    "message": "Success",
+    "info": {
+        "total": 1
+    },
+    "data": [
+        {
+            "id": 1,
+            "name": "gzeinnumer",
+            "email": "gzeinnumer@admin.com",
+            "email_verified_at": null,
+            "password": "12345678",
+            "remember_token": null,
+            "created_at": "2023-02-28 17:51:15",
+            "updated_at": "2023-02-28 17:51:15"
+        }
+    ]
+}'
+    ];
+    $data[] = [
+        'url' => 'api/users/update/1',
+        'request' => '{
+    "status": 1,
+    "title": "Perhatian",
+    "message": "Success"
+}',
+        'response' => '{
+    "status": 1,
+    "title": "Perhatian",
+    "message": "Success"
+}'
+    ];
+    $data[] = [
+        'url' => 'api/users/find/1',
+        'request' => '-',
+        'response' => '{
+    "status": 1,
+    "title": "Perhatian",
+    "message": "Success",
+    "info": {
+        "total": 1
+    },
+    "data": {
+        "id": 1,
+        "name": "gzeinnumer1",
+        "email": "gzeinnumer1@admin.com",
+        "email_verified_at": null,
+        "password": "12345678",
+        "remember_token": null,
+        "created_at": "2023-03-02 17:26:32",
+        "updated_at": "2023-03-02 17:26:32"
+    }
+}'
+    ];
+    $data[] = [
+        'url' => 'api/users/delete/{id}',
+        'request' => '-',
+        'response' => '{
+    "status": 1,
+    "title": "Perhatian",
+    "message": "Success"
+}'
+    ];
+    return view('welcome', ['data' => $data]);
 });
